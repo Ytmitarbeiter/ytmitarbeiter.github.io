235d1e71940eeb14d740551bead23f11fbf2d16f
BuildTools <unconfigured@null.spigotmc.org>
2020-09-21 18:58:01 +0200

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,13 +1,21 @@
-url = "http://ytmitarbeiter.github.io/geo/";
-var user = document.getElementById("username");
-var passw = document.getElementById("passwort");
-var pw = "123456789";
-    function checkit() {
-        if(passw == pw){
-            alert("Das Passwort ist Richtig. Sie werden weitergeleit zu: " + url);
-            window.location.href = url;
-            }
-            if(passw != pw) {
-                alert("Falsch!")
-            }
-        }
+var attempt = 3;
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
