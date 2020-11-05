d947fa89e27e6d00c4c110d960219e20ea0fa871
BuildTools <unconfigured@null.spigotmc.org>
2020-11-05 19:24:43 +0100

    betasystem

---
+++ betasystem/languages/lang_de.php
@@ -0,0 +1,23 @@
+<?php
+/*
+English Language File
+Made by Tutorialwork
+------------------------
+Add your own language by translating this messages.
+You can send the translated file with the name of the language to me to add the language for all in the next update.
+Email: TutorialworkTV@gmail.com
+*/
+define("SITE_TITLE", "Betakey einlösen");
+define("NOJS", "Bitte aktiviere Javascript!");
+define("SUCCESS_STATUS", "Erfolgreich");
+define("ERROR_STATUS", "Fehler");
+define("SUCCESS", "Du hast den Betakey erfolgreich eingelöst und kannst den Server nun betreten.");
+define("PLAYER_ALREADY_REDEEMED", "Du hast bereits einen Betakey eingelöst.");
+define("KEY_ALREADY_REDEEMED", "Dieser Betakey wurde bereits eingelöst.");
+define("KEY_INVALID", "Dieser Betakey ist ungültig.");
+define("SITE_HEADING", "Betazugang freischalten");
+define("SITE_TEXT", "Solltest du einen Key für den Betatest haben kannst du ihn hier aktivieren.");
+define("INPUT_PLAYER", "Dein Spielername");
+define("INPUT_KEY", "Betakey");
+define("INPUT_BUTTON", "Aktivieren");
+?>
